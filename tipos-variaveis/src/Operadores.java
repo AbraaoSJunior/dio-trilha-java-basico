50bf975e7839666eccba783f2928ec921aca8868
AbraaoSJunior <abraaoasjr@gmail.com>
2024-09-01 13:35:47 -0300

    correção

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,7 @@
 public class Operadores {
    
-    public static void main(String[] args){
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+    public static void main(String[] args) {
+        
 
         String concatenacao ="?";
 
